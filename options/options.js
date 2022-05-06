4c6b156ceee712f4bed68ecaa358b9c5e8c4c75d
Crowdbotics <team@crowdbotics.com>
2022-05-06 01:37:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "basketball_toons_35105",
+  url: "https://basketball_toons_35105.botics.co",
+  api: "https://basketball_toons_35105.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
